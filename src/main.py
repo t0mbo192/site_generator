5b773b0ba3a982dc8a0817fbfc6c529a267da5a0
t0mbo192 <tomlett192@gmail.com>
2026-02-17 05:48:35 -0500

    unittests implemented and working

---
+++ src/main.py
@@ -0,0 +1,7 @@
+from textnode import TextNode, TextType
+
+def main():
+    new_node =TextNode("Hello, World!", TextType.TEXT)
+    print(new_node)
+
+main()
---
+++ src/test_textnode.py
@@ -0,0 +1,23 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    def test_neq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.ITALIC)
+        self.assertNotEqual(node, node2)
+    
+    def test_url(self):
+        node = TextNode("This is a link", TextType.LINK, "https://example.com")
+        node2 = TextNode("This is a link", TextType.LINK)
+        self.assertNotEqual(node, node2)
+
+if __name__ == "__main__":
+    unittest.main()
