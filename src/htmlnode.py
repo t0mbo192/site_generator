03e8bfb36118faca078b37099232816ca2cc2aac
t0mbo192 <tomlett192@gmail.com>
2026-02-20 05:50:58 -0500

    added leafnode and tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -17,21 +17,25 @@ class HTMLNode:
         parts = []
 
         for key, value in self.props.items():
-            parts.append(f'{key}="{value}"')
-                
+            parts.append(f'{key}="{value}"')    
         return " " + " ".join(parts)
     
     def __repr__(self):
-        return f"HTMLNode(tag={self.tag}, value={self.value}, children={self.children}, props={self.props})"
+        return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
     
 class LeafNode(HTMLNode):
-    def __init__(self, tag=None, value=None, props=None):
+    def __init__(self, tag, value, props=None):
         super().__init__(tag=tag, value=value, children=None, props=props)
 
     def to_html(self):
         if self.value is None:
             raise ValueError("Invalid HTML: no value")
-        
+        if self.tag is None:
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+    
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
 
 
         
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,4 +1,20 @@
 import unittest
+from htmlnode import HTMLNode, LeafNode
 
-from htmlnode import HTMLNode
+class TestHTMLNode(unittest.TestCase):
+    def test_props_to_html_two_props(self):
+        node = HTMLNode(props={"href": "https://google.com", "target": "_blank"})
+        self.assertEqual(node.props_to_html(), ' href="https://google.com" target="_blank"')
+
+    def test_props_to_html_none(self):
+        node = HTMLNode(props=None)
+        self.assertEqual(node.props_to_html(), "")
+
+    def test_props_to_html_empty(self):
+        node = HTMLNode(props={})
+        self.assertEqual(node.props_to_html(), "")
+    
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
 
